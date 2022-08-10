1f251e69ddc790a6cb0508a367862012f64e1d01
Asad <usasad99@gmail.com>
2022-08-10 13:55:12 +0500

    added required changes #39

--- src/Components/Partner/Partner.jsx
+++ src/Components/Partner/Partner.jsx
@@ -23,31 +23,33 @@ const Partner = () => {
         <div className="splides-top">
           <h2 className='splides-title'>{t("home_page_partners_title")}</h2>
         </div>
-        {partners.length > 0 && (
-          <Splide
-            className='splide-slide'
-            options={{
-              type: 'loop', perPage:
-                4, autoplay: true,
-            }}>
-            {partners.map(partner => (
-              <SplideSlide key={partner.id}
-                className='splide-box'>
-                <span className="splide-link">
-                  <img className='splide-img' src={partner.image}
-                    alt={`Partner img${partner.id}`} />
-                </span>
-              </SplideSlide>
-            ))}
-          </Splide>
-        )}
+        <div className='splides-container'>
+          {partners.length > 0 && (
+            <Splide
+              className='splide-slide'
+              options={{
+                type: 'loop', perPage:
+                  4, autoplay: true,
+              }}>
+              {partners.map(partner => (
+                <SplideSlide key={partner.id}
+                  className='splide-box'>
+                  <span className="splide-link">
+                    <img className='splide-img' src={partner.image}
+                      alt={`Partner img${partner.id}`} />
+                  </span>
+                </SplideSlide>
+              ))}
+            </Splide>
+          )}
+        </div>
         <div className="splides-box">
           {partners.length > 0 && (
             <ul className="splides-list">
-              {partners.map((partner, i) => (
-                <li className="splides-item" key={i}>
+              {partners.map(partner => (
+                <li className="splides-item" key={partner.id}>
                   <span className="splides-link">
-                    <img src={Adidas} alt=""
+                    <img src={partner.image} alt=""
                       className="splides-img" />
                   </span>
                 </li>
